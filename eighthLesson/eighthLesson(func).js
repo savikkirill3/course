65f17d1486426ab0ab9774e886ff8639da76458b
savikkirill3 <39432789+savikkirill3@users.noreply.github.com>
2018-06-29 16:36:29 +0300

    Add eighthLesson(func).js

--- eighthLesson/eighthLesson(func).js
+++ eighthLesson/eighthLesson(func).js
@@ -1,37 +1,39 @@
 function  Animal(name, kg) {
-    this.name = name;
-    this.kg = kg;
+    var name = name;
+    var kg = kg;
     this.run = function () {
         console.log(name  + ` runs`)
     };
-    function eat() {
+    var eat = function() {
         return kg;
     };
     this.howManyEat = function () {
-        console.log( this.name + ` ест ` + eat() + ` килограмм`);
+        console.log( name + ` ест ` + eat() + ` килограмм`);
+    }
+    function getName() {
+        return name;
+    }
+}
+
+function Cat(name, kg) {
+    Animal.apply(this, arguments);
+    this.catEat = function () {
+        console.log(`Cat’s name is ` + name);
+    }
+}
+
+function Dog(name, kg) {
+    Animal.apply(this, arguments);
+    this.dogEat = function () {
+        console.log(`Dog’s name is ` + name);
     }
 }
 var animal = new Animal('molly', 5);
 animal.run();
 animal.howManyEat();
 
-
-function Cat(name, kg) {
-    Animal.apply(this, arguments);
-    this.catEat = function () {
-        console.log(`Cat’s name is ` + this.name);
-    }
-}
-
-function Dog() {
-    Animal.apply(this, arguments);
-    this.dogEat = function () {
-        console.log(`Dog’s name is ` + dog.name);
-    }
-}
 var cat = new Cat(`вася`, 3);
 var dog = new Dog(`шарик`, 10);
-
 cat.catEat();
 dog.dogEat();
 cat.howManyEat();
