c3c1609b387ee8c962efba1e12dc9a511a69d072
savikkirill3 <39432789+savikkirill3@users.noreply.github.com>
2018-07-11 14:21:32 +0300

    Add sixthLesson.js

--- sixthLesson/sixthLesson.js
+++ sixthLesson/sixthLesson.js
@@ -1,16 +1,21 @@
 
-const array = [[1,2,3], true, null, 123, {"a": "b","c": "d","e": "f"}];
+const array = [[1,2,3], true, null, 123, {"a": "b","c": "d","e": "f"}, this];
 
 function tryToParseJSON(arr) {
 
     var massJSON = [];
     array.forEach(function (item) {
-        massJSON.push(JSON.stringify(item))
+        try {
+            massJSON.push(JSON.stringify(item))
+        }
+        catch (e) {
+            massJSON.push('Ошибка ' + e.name + ":" + e.message);
+        }
     });
     return massJSON;
 }
 setTimeout(function () {
 
-   console.log(tryToParseJSON(array))
+   tryToParseJSON(array);
 
 }, 5000);
